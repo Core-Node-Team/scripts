96ced2d11e2915fff3e869255d66bf368a772b93
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-12-21 15:04:35 +0300

    Create install.sh

---
+++ babylon/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source <(curl -s https://raw.githubusercontent.com/0xSocrates/Scripts/main/style.sh)
+source <(curl -s https://raw.githubusercontent.com/0xSocrates/Scripts/main/general_functions.sh)
+source <(curl -s https://raw.githubusercontent.com/Core-Node-Team/scripts/main/babylon/config.sh)
+clear
+logo
+get_moniker
+removenode
+prepare_server
+install_binary
+init
+config
+snapshot
+service
+lastinfo
+source $HOME/.bash_profile
