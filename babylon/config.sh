4f476050ed3bf71501aef09aed94699d16ae9e4c
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-12-23 19:29:37 +0300

    Update config.sh

--- babylon/config.sh
+++ babylon/config.sh
@@ -20,7 +20,7 @@ sleep 1
 
 snapshot() {
 print_color $Blue "Snapshot İndiriliyor..."
-curl -L http://37.120.189.81/babylon_testnet/babylon_snap.tar.lz4 | tar -I lz4 -xf - -C $HOME/.babylond/data
+curl -L http://37.120.189.81/babylon_testnet/babylon_snap.tar.lz4 | tar -I lz4 -xf - -C $HOME/.babylond
 }
 
 config() {
