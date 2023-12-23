d3ea210c82e867eab952c8c58f58ce67d1feee4f
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-12-23 19:30:02 +0300

    Update config.sh

--- elys/config.sh
+++ elys/config.sh
@@ -19,7 +19,7 @@ sleep 1
 
 snapshot() {
 print_color $Blue "Snapshot İndiriliyor..."
-curl -L http://37.120.189.81/elys_testnet/elys_snap.tar.lz4 | tar -I lz4 -xf - -C $HOME/.elys/data
+curl -L http://37.120.189.81/elys_testnet/elys_snap.tar.lz4 | tar -I lz4 -xf - -C $HOME/.elys
 }
 
 config() {
