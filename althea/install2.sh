c4bdbc72fff3111388e16bad3c728f2fbae1dede
molla202 <91562185+molla202@users.noreply.github.com>
2023-12-29 01:42:36 +0300

    Create install2.sh

---
+++ althea/install2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source <(curl -s https://raw.githubusercontent.com/0xSocrates/Scripts/main/style.sh)
+source <(curl -s https://raw.githubusercontent.com/0xSocrates/Scripts/main/general_functions.sh)
+source <(curl -s https://raw.githubusercontent.com/Core-Node-Team/scripts/main/althea/config2.sh)
+clear
+logo
+get_moniker
+removenode
+prepare_server
+install_binary
+init
+config
+snapshot
+service
+lastinfo
+source $HOME/.bash_profile
+
+
+
