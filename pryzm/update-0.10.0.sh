0ea677d58a5e5bf79d0cc058f7e613da3355a9d4
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-12-28 16:11:14 +0300

    Update update-0.10.0.sh

--- pryzm/update-0.10.0.sh
+++ pryzm/update-0.10.0.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 source <(curl -s https://raw.githubusercontent.com/0xSocrates/Scripts/main/style.sh)
 clear
-
 HedefBlok="316000"
-
 update() {
 print_color $Green "Düğüm belirtilen blok yüksekliğine ulaştı. Güncelleme yapılıyor..."
 sudo systemctl stop pryzmd
-
-
 MIMARI=$(uname -m)
 
 if [ "$MIMARI" = "x86_64" ]; then
@@ -31,7 +27,6 @@ sudo systemctl restart pryzmd
 print_color $Blue "Güncelleme tamamldandı. Version: $(pryzmd version)" sleep 1
 print_color $Blue "Logları takip edin:         sudo journalctl -u pryzmd -fo cat" sleep 1
 }
-
 curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/core-node.sh | bash
 echo -e ""
 echo -e "Current Version: $(pryzmd version)"
@@ -39,7 +34,6 @@ echo -e "Update Version: 0.10.0"
 echo -e "Update Height: $HedefBlok"
 echo -e "Current Height: $(pryzmd status 2>&1 | jq -r .SyncInfo.latest_block_height)"
 echo -e ""
-
 while true; do
     latest_block_height=$(pryzmd status 2>&1 | jq -r .SyncInfo.latest_block_height)
     if [ "$latest_block_height" -ge "$HedefBlok" ]; then
@@ -50,5 +44,4 @@ while true; do
     fi
     sleep 1
 done
-
 source $HOME/.bash_profile
