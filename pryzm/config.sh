5500a507b5d9431efad5c8d8657db5ea2d21970c
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-12-29 16:25:51 +0300

    Update config.sh

--- pryzm/config.sh
+++ pryzm/config.sh
@@ -34,7 +34,7 @@ sleep 1
 
 snapshot() {
 print_color $Blue "Snapshot İndiriliyor..."
-curl -L http://37.120.189.81/pryzm_testnet/pryzm_snap.tar.lz4 | tar -I lz4 -xf - -C $HOME/.pryzm/data
+curl -L http://37.120.189.81/pryzm_testnet/pryzm_snap.tar.lz4 | tar -I lz4 -xf - -C $HOME/.pryzm
 }
 
 config() {
