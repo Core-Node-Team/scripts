cf8c2fe4f0eb10e2675ec8861abde1e497ac125f
molla202 <91562185+molla202@users.noreply.github.com>
2024-01-13 13:12:30 +0300

    Update install.sh

--- artela/install.sh
+++ artela/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source <(curl -s https://raw.githubusercontent.com/0xSocrates/Scripts/main/style.sh)
 source <(curl -s https://raw.githubusercontent.com/0xSocrates/Scripts/main/general_functions.sh)
-source <(curl -s https://raw.githubusercontent.com/Core-Node-Team/scripts/main/babylon/config.sh)
+source <(curl -s https://raw.githubusercontent.com/Core-Node-Team/scripts/main/artela/config.sh)
 clear
 logo
 get_moniker
